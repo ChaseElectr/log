8bc572f7d5ac89ea7e937b1afa41ffde5eafdfa5
张琦 <zhangqi.ruster@bytedance.com>
2019-08-25 21:58:01 +0800

    feat: add emoji log level

--- src/lib.rs
+++ src/lib.rs
@@ -323,6 +323,8 @@ static MAX_LOG_LEVEL_FILTER: AtomicUsize = ATOMIC_USIZE_INIT;
 
 static LOG_LEVEL_NAMES: [&'static str; 6] = ["OFF", "ERROR", "WARN", "INFO", "DEBUG", "TRACE"];
 
+static LOG_LEVEL_EMOJIES: [&'static str; 6] = ["", "❤️", "💛", "💙", "💚", "💜"];
+
 static SET_LOGGER_ERROR: &'static str = "attempted to set a logger after the logging system \
                                          was already initialized";
 static LEVEL_PARSE_ERROR: &'static str =
@@ -499,6 +501,11 @@ impl Level {
             5 => Some(Level::Trace),
             _ => None,
         }
+    }
+
+    /// Returns emoji representation of logging level
+    pub fn emoji(&self) -> &'static str {
+        LOG_LEVEL_EMOJIES[*self as usize]
     }
 
     /// Returns the most verbose logging level.
